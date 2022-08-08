c2fbd23ccd570f5a96a0c32e4e23ef8d14719466
Daniel Ferrada <startel.temuco@gmail.com>
2022-08-08 11:53:09 -0400

    se elimina requerido en instruct

--- models/practicas.py
+++ models/practicas.py
@@ -114,6 +114,6 @@ class CertInstructor(models.Model):
 
     name = fields.Char(string='Nombre', required=True )
     date = fields.Date(string='Fecha', required=True)
-    card = fields.Many2one('fleet.vehicle.model.brand', required=True)
-    model = fields.Char(string='Modelo', required=True)
-    patente = fields.Char(string='Patente', required=True)
+    card = fields.Many2one('fleet.vehicle.model.brand')
+    model = fields.Char(string='Modelo')
+    patente = fields.Char(string='Patente')
